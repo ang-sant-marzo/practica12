7a7abc1787d8512efe38bb58daf36859de25c34e
ang-sant-marzo <ang.sant.marzo@gmail.com>
2021-03-12 09:17:55 +0000

    ends

--- src/app/servicios/productos.service.ts
+++ src/app/servicios/productos.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
+  productos: any = [];
+  private contador = new Subject<any>()
+
   constructor() { }
+
+  addProducto (producto: any): void {
+    this.productos.push(producto);
+    this.contador.next({numeroProductos: this.productos.length})
+  }
+
+  getNumeroProductos(): Observable<any> {
+    return this.contador.asObservable();
+  }
+
 }
+// 
